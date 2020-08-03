90025e4a0c26cae946b347c71ece63e60d9ff3a6
Chris <boyd111c@gmail.com>
2020-08-02 20:05:21 -0600

    modified grid material to allow highlight frequency fixed color gaps in grid

--- docs/iwo/src/materials/GridMaterial.js
+++ docs/iwo/src/materials/GridMaterial.js
@@ -2,15 +2,17 @@ import { Renderer as Renderer$1 } from '../graphics/Renderer.js';
 import { Material as Material$1 } from './Material.js';
 
 class GridMaterial extends Material$1 {
-    constructor(view_distance, frequency = 1) {
+    constructor(view_distance, frequency = 1, highlight_frequency = 10) {
         super();
         this.distance = view_distance;
         this.frequency = frequency;
+        this.highlight_frequency = highlight_frequency;
     }
     activate(gl) {
         let shader = this.shader;
         shader.setUniform("distance", this.distance);
         shader.setUniform("frequency", this.frequency);
+        shader.setUniform("highlight_frequency", this.highlight_frequency);
     }
     get shader() {
         return Renderer$1.GetShader("GridShader");
