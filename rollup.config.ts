a1d45a308ecd335c9b0e3f7d6bfe562100bbbbb1
Chris <boyd111c@gmail.com>
2022-12-03 18:00:00 -0600

    enable bundling for build

--- rollup.config.ts
+++ rollup.config.ts
@@ -35,7 +35,7 @@ const output_dir = "docs";
 
 export default {
     //NOTE: Enable node_modules and src imports to keep in original files and location
-    preserveModules: true,
+    //preserveModules: true,
     input: Object.keys(examples).map((key) => `examples/${key}.ts`),
     output: {
         dir: output_dir,
@@ -61,7 +61,7 @@ export default {
                 fileName: `examples/${key}.html`,
                 template: ({ attributes, bundle, files, publicPath, title }) => {
                     const html = template
-                        .replace("</body>", `<script type="module" src="./${key}.js"></script></body>`)
+                        .replace("</body>", `<script type="module" src="../${key}.js"></script></body>`)
                         .replace("</title>", `${examples[key]}</title>`);
                     return html;
                 },
@@ -69,7 +69,7 @@ export default {
         ),
         glslify({
             //compress removes spaces and new line breaks after keywords like 'else' breaking shaders without braces
-            compress: false,
+            compress: true,
         }),
         copy({
             targets: [
